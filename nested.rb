c517af98ee2a6d2fbbd0b87f5a82d12d5a7668ab
Ronald Stephens <rstephens@bluemorpho.cc>
2019-07-30 16:54:54 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -2,6 +2,11 @@
 def hopper
 	programmer_hash =
  		{
+        :yukihiro_matsumoto => {
+        :known_for => "Ruby",
+        :languages => ["LISP", "C"]
+        }
+      
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
